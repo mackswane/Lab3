21acc0a16272fa8c37e29e09542a8f7275115d09
mackswane <31156711+mackswane@users.noreply.github.com>
2017-09-01 20:38:25 +0530

    Create lab3question5.cpp

---
+++ lab3question5.cpp
@@ -0,0 +1,21 @@
+# Lab3
+//question5
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a;
+    cout << "\nenter a no.=";
+    cin>>a;
+    if(a==0)
+      {cout<<"\nyour no. is zero";}
+        else
+    if(a%2==0)
+      {cout<<"\nyour no. is even";}
+        else
+          cout<<"\nyour no. is odd";
+
+    return 0;
+}
