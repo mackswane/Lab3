4eb5f58c4606a3a11520d9dcb39fb2b2e8f84604
mackswane <31156711+mackswane@users.noreply.github.com>
2017-09-01 20:13:49 +0530

    Create lab3question3.cpp

---
+++ lab3question3.cpp
@@ -0,0 +1,19 @@
+# Lab3
+//question3
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a;
+    cout << "\nenter a no.=";
+    cin>>a;
+    if(a>0)
+      {cout<<"\nyour no. is positive";}
+      else
+      if(a<0)
+        {cout<<"\nyour no. is negative";}
+        else cout<<"\nyour no. is zero";
+    return 0;
+}
