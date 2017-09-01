a0766bb1b446ae6b9394946d50b570b31d85d2f6
mackswane <31156711+mackswane@users.noreply.github.com>
2017-09-01 20:00:25 +0530

    Create lab3question1.cpp

---
+++ lab3question1.cpp
@@ -0,0 +1,20 @@
+# Lab3
+//question1
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a,b;
+    cout << "\nenter 1st no.=";
+    cin>>a;
+    cout<<"\nenter 2nd no.=";
+    cin>>b;
+    if(a>b)
+      {
+      cout<<"\n"<<a<<" is greater than"<<b;
+      }
+      else{cout<<"\n"<<b<<" is greater than"<<a;}
+    return 0;
+}
