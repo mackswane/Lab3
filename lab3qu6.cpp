0e7bb1ab579dd31eb106cea68365345091473258
mackswane <31156711+mackswane@users.noreply.github.com>
2017-09-03 14:12:22 +0530

    Create lab3qu6.cpp

---
+++ lab3qu6.cpp
@@ -0,0 +1,18 @@
+# Lab3
+//qu6
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int y;
+    cout << "\nenter year= ";
+    cin>>y;
+    if(y%4==0)
+    cout<<"\nyear entered is a leap year";
+    else
+    cout<<"\n year enetered is not a leap year";
+    return 0;
+}
+
