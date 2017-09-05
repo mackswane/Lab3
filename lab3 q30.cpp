6a528fc472cb64b89031bfeaaf0abcf42050717b
mackswane <31156711+mackswane@users.noreply.github.com>
2017-09-05 17:11:19 +0530

    Create lab3 q30.cpp

---
+++ lab3 q30.cpp
@@ -0,0 +1,15 @@
+# Lab3
+//ques 30
+#include<iostream>
+using namespace std;
+int main()
+{
+ int n,i=1;
+cout<<"\nenter the number you want to print multiplication table:";
+ cin>>n;
+ while(i<=10){
+      cout<<"\n"<<n<<"X"<<i<<"="<<n*i;
+      i++;
+ }
+ return 0;
+}
