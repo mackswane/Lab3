0ad8daf8c0795677e3bc17c781fc20b1e30cd5d7
mackswane <31156711+mackswane@users.noreply.github.com>
2017-09-05 16:19:45 +0530

    Create lab3 q23.cpp

---
+++ lab3 q23.cpp
@@ -0,0 +1,15 @@
+# Lab3
+//ques 23
+#include<iostream>
+using namespace std;
+int main()
+{
+ int n;
+ cout<<"\nenter the limit upto you want to print natural numbers: ";
+ cin>>n;
+ while(n>0){
+      cout<<"\t"<<n;
+      n--;
+ }
+ return 0;
+}
