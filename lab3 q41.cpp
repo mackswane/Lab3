1b16a9d532a3ecb3d949f723cbadba175f90623a
mackswane <31156711+mackswane@users.noreply.github.com>
2017-09-07 18:53:49 +0530

    Create lab3 q41.cpp

---
+++ lab3 q41.cpp
@@ -0,0 +1,16 @@
+# Lab3
+//quse 41
+#include<iostream>
+using namespace std;
+int main()
+{
+ int i=0;
+ cout<<"Values"<<"    "<<"ASCII CODES";
+ while(i<256){
+      cout<<"\n"<<i<<"\t"<<char(i);
+      i++;
+
+ }
+
+ return 0;
+}
